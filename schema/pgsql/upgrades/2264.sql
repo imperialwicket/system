ec7bbb2233ce38a6061c8433b391eb44ec2a5d53
Blake Johnson <bjohnson@contrib.habariproject.org>
2008-09-11 23:50:21 +0000

    schema06: Applying enn's patch for PostgreSQL. Does it work now?
    git-svn-id: http://svn.habariproject.org/habari/branches/schema06/system@2478 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- schema/pgsql/upgrades/2264.sql
+++ schema/pgsql/upgrades/2264.sql
@@ -1,8 +1,12 @@
-ALTER TABLE {$prefix}posts MODIFY pubdate VARCHAR(25) NOT NULL;
-ALTER TABLE {$prefix}posts MODIFY updated VARCHAR(25) NOT NULL;
+ALTER TABLE {$prefix}posts ALTER COLUMN pubdate TYPE VARCHAR(25);
+ALTER TABLE {$prefix}posts ALTER COLUMN pubdate SET NOT NULL;
+ALTER TABLE {$prefix}posts ALTER COLUMN updated TYPE VARCHAR(25);
+ALTER TABLE {$prefix}posts ALTER COLUMN updated SET NOT NULL;
 ALTER TABLE {$prefix}posts ADD COLUMN modified INT UNSIGNED NOT NULL;
-ALTER TABLE {$prefix}comments MODIFY date VARCHAR(25) NOT NULL;
-ALTER TABLE {$prefix}log MODIFY timestamp VARCHAR(25) NOT NULL;
+ALTER TABLE {$prefix}comments ALTER COLUMN date TYPE VARCHAR(25);
+ALTER TABLE {$prefix}comments ALTER COLUMN date SET NOT NULL;
+ALTER TABLE {$prefix}log ALTER COLUMN timestamp TYPE VARCHAR(25);
+ALTER TABLE {$prefix}log ALTER COLUMN timestamp SET NOT NULL;
 
 UPDATE {$prefix}posts SET pubdate = UNIX_TIMESTAMP(pubdate);
 UPDATE {$prefix}posts SET updated = UNIX_TIMESTAMP(updated);
@@ -10,16 +14,23 @@ UPDATE {$prefix}posts SET modified = updated;
 UPDATE {$prefix}comments SET date = UNIX_TIMESTAMP(date);
 UPDATE {$prefix}log SET timestamp = UNIX_TIMESTAMP(timestamp);
 
-ALTER TABLE {$prefix}posts MODIFY pubdate INT UNSIGNED NOT NULL;
-ALTER TABLE {$prefix}posts MODIFY updated INT UNSIGNED NOT NULL;
-ALTER TABLE {$prefix}comments MODIFY date INT UNSIGNED NOT NULL;
-ALTER TABLE {$prefix}log MODIFY timestamp INT UNSIGNED NOT NULL;
+ALTER TABLE {$prefix}posts ALTER COLUMN pubdate TYPE INT;
+ALTER TABLE {$prefix}posts ALTER COLUMN pubdate SET NOT NULL;
+ALTER TABLE {$prefix}posts ALTER COLUMN updated TYPE INT;
+ALTER TABLE {$prefix}posts ALTER COLUMN updated SET NOT NULL;
+ALTER TABLE {$prefix}comments ALTER COLUMN date TYPE INT;
+ALTER TABLE {$prefix}comments ALTER COLUMN date SET NOT NULL;
+ALTER TABLE {$prefix}log ALTER COLUMN timestamp TYPE INT;
+ALTER TABLE {$prefix}log ALTER COLUMN timestamp SET NOT NULL;
 
-ALTER TABLE {$prefix}crontab MODIFY COLUMN last_run INT UNSIGNED;
-ALTER TABLE {$prefix}crontab MODIFY COLUMN next_run INT UNSIGNED NOT NULL;
-ALTER TABLE {$prefix}crontab MODIFY COLUMN increment INT UNSIGNED NOT NULL;
-ALTER TABLE {$prefix}crontab MODIFY COLUMN start_time INT UNSIGNED NOT NULL;
-ALTER TABLE {$prefix}crontab MODIFY COLUMN end_time INT UNSIGNED;
+ALTER TABLE {$prefix}crontab ALTER COLUMN last_run TYPE INT;
+ALTER TABLE {$prefix}crontab ALTER COLUMN next_run TYPE INT;
+ALTER TABLE {$prefix}crontab ALTER COLUMN next_run SET NOT NULL;
+ALTER TABLE {$prefix}crontab ALTER COLUMN increment TYPE INT;
+ALTER TABLE {$prefix}crontab ALTER COLUMN increment SET NOT NULL;
+ALTER TABLE {$prefix}crontab ALTER COLUMN start_time TYPE INT;
+ALTER TABLE {$prefix}crontab ALTER COLUMN start_time SET NOT NULL;
+ALTER TABLE {$prefix}crontab ALTER COLUMN end_time TYPE INT;
 
 UPDATE {$prefix}crontab SET last_run=NULL WHERE last_run=0;
 UPDATE {$prefix}crontab SET end_time=NULL WHERE end_time=0;
